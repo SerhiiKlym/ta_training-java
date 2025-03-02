749ae942f43e9333ca6c54f6fe27379c1e55760c
SerhiiKlymenko <mafakka@mail.ru>
2025-03-01 20:12:52 -0500

    test data handlers

---
+++ app/src/test/java/com/epam/training/serhii_klymenko/task1/JsonDataUtility.java
@@ -0,0 +1,18 @@
+package com.epam.training.serhii_klymenko.task1;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public class JsonDataUtility {
+    private static final String JSON_FILE_PATH = "src/test/resources/test-data.json";
+
+    protected static Map<String, String> getTestData() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new File(JSON_FILE_PATH), new TypeReference<>() {
+        });
+    }
+}
