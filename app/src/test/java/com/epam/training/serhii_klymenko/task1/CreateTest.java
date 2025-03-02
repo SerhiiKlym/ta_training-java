69dfea16797bb9510684ae714cecc299fe8ad8bf
SerhiiKlymenko <mafakka@mail.ru>
2025-03-02 09:51:30 -0500

    the test itself, create test

---
+++ app/src/test/java/com/epam/training/serhii_klymenko/task1/CreateTest.java
@@ -0,0 +1,26 @@
+package com.epam.training.serhii_klymenko.task1;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.testng.Assert.assertTrue;
+
+public class CreateTest extends BaseTest {
+    @Test
+    public void testCreatingSimplePaste() throws IOException {
+        driver.get("https://pastebin.com/");
+        PasteBinMainPage mainPage = new PasteBinMainPage(driver);
+        Map<String, String> testData = JsonDataUtility.getTestData();
+
+        mainPage.clickCreateNewPasteBtn();
+        mainPage.inputCodeIntoPostForm(testData.get("post"));
+        mainPage.clickPasteExpirationDropdown();
+        mainPage.clickTenMinutesExpiration();
+        mainPage.insertPasteName(testData.get("pasteName"));
+        mainPage.clickSubmitNewPaste();
+
+        assertTrue(mainPage.isSuccessNoticeDisplayed(), testData.get("failMsg"));
+    }
+}
