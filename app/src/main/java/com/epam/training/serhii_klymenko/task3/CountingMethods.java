8e03ab285757d58b4c004c58f373224115d0fd82
SerhiiKlymenko <mafakka@mail.ru>
2025-03-04 17:16:04 -0500

    maxNumberUnequalConsecutiveIdenticalDigitChars method

--- app/src/main/java/com/epam/training/serhii_klymenko/task3/CountingMethods.java
+++ app/src/main/java/com/epam/training/serhii_klymenko/task3/CountingMethods.java
@@ -69,10 +69,34 @@ public class CountingMethods {
     }
 
     public int maxNumberUnequalConsecutiveIdenticalDigitChars(String inputString) {
-        //placeholder for TDD process
         Integer x = checkForEmptyNullSingleChar(inputString);
         if (x != null) return x;
-        return 0;
+
+        int result = 0;
+        int tmpResult = 1;
+        boolean hasDifferentChars = false;
+        Pattern pattern = Pattern.compile("\\d");
+
+        for (int i = 1; i < inputString.length(); i++) {
+            char prevChar = inputString.charAt(i - 1);
+            char currChar = inputString.charAt(i);
+
+            boolean isPrevLatin = pattern.matcher(String.valueOf(prevChar)).matches();
+            boolean isCurrentLatin = pattern.matcher(String.valueOf(currChar)).matches();
+
+            if (isPrevLatin && isCurrentLatin && prevChar != currChar) {
+                tmpResult++;
+                hasDifferentChars = true;
+            } else {
+                if (tmpResult > result) {
+                    result = tmpResult;
+                }
+                tmpResult = 1;
+            }
+        }
+        result = Math.max(result, tmpResult);
+        return hasDifferentChars ? result : 0;
+
     }
 
 }
