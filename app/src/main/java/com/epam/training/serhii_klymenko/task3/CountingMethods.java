e5aaa90c38c189972e9e1ee83e49239000dc7af8
SerhiiKlymenko <mafakka@mail.ru>
2025-03-04 09:37:20 -0500

    added second counting method, refactored test names for the first method.

--- app/src/main/java/com/epam/training/serhii_klymenko/task3/CountingMethods.java
+++ app/src/main/java/com/epam/training/serhii_klymenko/task3/CountingMethods.java
@@ -1,7 +1,10 @@
 package com.epam.training.serhii_klymenko.task3;
 
+import java.util.regex.Pattern;
+
 public class CountingMethods {
-    public int maxNumberUnequalConsecutiveChars(String inputString) {
+
+    private static Integer checkForEmptyNullSingleChar(String inputString) {
         if (inputString == null || inputString.length() == 0) {
             return 0;
         }
@@ -9,6 +12,12 @@ public class CountingMethods {
         if (inputString.length() == 1) {
             return 1;
         }
+        return null;
+    }
+
+    public int maxNumberUnequalConsecutiveChars(String inputString) {
+        Integer x = checkForEmptyNullSingleChar(inputString);
+        if (x != null) return x;
 
         int result = 0;
         int tmpResult = 1;
@@ -28,8 +37,42 @@ public class CountingMethods {
         return hasDifferentChars ? result : 0;
     }
 
-    public int maxNumberUnequalConsecutiveIdenticalLatinAlphabetChars(String inputString) {
-        //placeholder to TDD process
+
+    public int maxNumberEqualConsecutiveIdenticalLatinAlphabetChars(String inputString) {
+        Integer x = checkForEmptyNullSingleChar(inputString);
+        if (x != null) return x;
+
+        int result = 0;
+        int tmpResult = 1;
+        boolean hasDifferentChars = false;
+        Pattern pattern = Pattern.compile("[A-Za-z]");
+
+        for (int i = 1; i < inputString.length(); i++) {
+            char prevChar = inputString.charAt(i - 1);
+            char currChar = inputString.charAt(i);
+
+            boolean isPrevLatin = pattern.matcher(String.valueOf(prevChar)).matches();
+            boolean isCurrentLatin = pattern.matcher(String.valueOf(currChar)).matches();
+
+            if (isPrevLatin && isCurrentLatin && prevChar == currChar) {
+                tmpResult++;
+                hasDifferentChars = true;
+            } else {
+                if (tmpResult > result) {
+                    result = tmpResult;
+                }
+                tmpResult = 1;
+            }
+        }
+        result = Math.max(result, tmpResult);
+        return hasDifferentChars ? result : 0;
+    }
+
+    public int maxNumberUnequalConsecutiveIdenticalDigitChars(String inputString) {
+        //placeholder for TDD process
+        Integer x = checkForEmptyNullSingleChar(inputString);
+        if (x != null) return x;
         return 0;
     }
+
 }
--- app/src/test/java/com/epam/training/serhii_klymenko/task3/CountingMethodsTest.java
+++ app/src/test/java/com/epam/training/serhii_klymenko/task3/CountingMethodsTest.java
@@ -14,63 +14,63 @@ public class CountingMethodsTest {
     }
 
     @Test
-    public void testEmptyString() {
+    public void testUnequalConsecutiveChars_EmptyString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars(""), 0, "Empty string doesn't produce '0' as a result");
     }
 
     @Test
-    public void testNullString() {
+    public void testUnequalConsecutiveChars_NullString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars(null), 0, "Null string doesn't produce '0' as a result");
     }
 
     @Test
-    public void testEntirelyUniqueCharactersString() {
+    public void testUnequalConsecutiveChars_AllUniqueChars() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("abcdefg"), 7, "Entirely unique string should count the longest sequence correctly.");
     }
 
     @Test
-    public void testEntirelyUniqueCharactersStringWithSymbols() {
+    public void testUnequalConsecutiveChars_AllUniqueCharactersStringWithSymbols() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("123!_@#%$kji9)"), 14, "A string with numbers and symbols should return its full length.");
     }
 
     @Test
-    public void testAllCharsEqualString() {
+    public void testUnequalConsecutiveChars_AllCharsEqualString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("sssssss"), 0, "All the same characters in the string should produce '0' as a result");
     }
 
     @Test
-    public void testOneCharacterString() {
+    public void testUnequalConsecutiveChars_OneCharacterString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("A"), 1, "One character string should count the longest sequence correctly");
     }
 
     @Test
-    public void testRepeatingUniqueSequenceString() {
+    public void testUnequalConsecutiveChars_RepeatingUniqueSequenceString() {
         String testString = "ab".repeat(100);
         assertEquals(instance.maxNumberUnequalConsecutiveChars(testString), 200, "Alternating characters should return the full length.");
     }
 
     @Test
-    public void testStartsWithRepetitionString() {
+    public void testUnequalConsecutiveChars_StartsWithRepetitionString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("SSSasdfghj"), 8, "A string starting with repeated characters should count the longest sequence correctly.");
     }
 
     @Test
-    public void testEndsWithRepetitionString() {
+    public void testUnequalConsecutiveChars_EndsWithRepetitionString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("abcdefgSSS"), 8, "A string ending with repeated characters should count the longest sequence correctly.");
     }
 
     @Test
-    public void testRepetitionInTheMiddleString() {
+    public void testUnequalConsecutiveChars_RepetitionInTheMiddleString() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("asdfghjSSSjgkg"), 8, "A string with repeated characters in the middle should count the longest sequence correctly.");
     }
 
     @Test
-    public void testStringWithSpaces() {
+    public void testUnequalConsecutiveChars_StringWithSpaces() {
         assertEquals(instance.maxNumberUnequalConsecutiveChars("asd bfg  lk "), 8, "A string with spaces should count the longest sequence correctly.");
     }
 
     @Test
-    public void testLongString() {
+    public void testUnequalConsecutiveChars_LongString() {
         String longInput = "abcdefgi".repeat(1000);
         assertEquals(instance.maxNumberUnequalConsecutiveChars(longInput), 8000, "A long unique character string should return its full length.");
     }
